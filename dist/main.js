3cd7057ef554c93d469422d64b2de659d2318ce1
Eduard Gavrila <edygavrila@gmail.com>
2021-09-10 18:34:10 +0100

    Adjusted image paths

--- dist/main.js
+++ dist/main.js
@@ -3,37 +3,37 @@
   const e = [
       {
         name: "Köttbullar – Meatballs",
-        photo: "../assets/Meatballs.jpg",
+        photo: "./assets/Meatballs.jpg",
         description:
           "Mixed and fried in a generous amount of butter, it’s traditionally served with mashed potato, lingonberry jam, and brown cream sauce.",
       },
       {
         name: "Räkmacka – Shrimp Sandwich",
-        photo: "../assets/Rakmacka.jpg",
+        photo: "./assets/Rakmacka.jpg",
         description:
           "Räkmacka is an open-faced sandwich, generally served on rye bread. It’s topped with lettuce, mayonnaise, egg, dill, caviar, and a generous amount of shrimp.",
       },
       {
         name: "Smulpaj – Crumble",
-        photo: "../assets/Smulpaj.jpg",
+        photo: "./assets/Smulpaj.jpg",
         description:
           "We love to use the fruit and berries for cooking and baking. Smulpaj is a summer favorite, and it’s made with whatever berries or fruits are in season. ",
       },
       {
         name: "Ärtsoppa & Pannkakor – Pea Soup & Pancakes",
-        photo: "../assets/Artsoppa.jpg",
+        photo: "./assets/Artsoppa.jpg",
         description:
           "The pea soup is made from dried yellow peas and pork broth, seasoned with thyme, and topped with salty pieces of pork and mustard. This is often served with a warm punsch drink.",
       },
       {
         name: " Smörgåstårta – Swedish Sandwich Cake",
-        photo: "../assets/Smorgstarta.jpg",
+        photo: "./assets/Smorgstarta.jpg",
         description:
           "Smörgåstårta is Sweden’s number one feast meal. This is where we put everything we like on bread, in layers, and serve it as a cake.",
       },
       {
         name: "Semla – Sweet Roll",
-        photo: "../assets/Semla.jpg",
+        photo: "./assets/Semla.jpg",
         description:
           "This pastry consists of a simple wheat bun, flavored with cardamom. The top is cut off, and it’s filled with almond paste and whipped cream. ",
       },
@@ -118,7 +118,7 @@
       (n.innerText = "The best food this part of Sweden!"),
         n.classList.add("tab-subtitle");
       const a = document.createElement("img");
-      (a.src = "../assets/home-image.jpg"), a.classList.add("home-image");
+      (a.src = "./assets/home-image.jpg"), a.classList.add("home-image");
       const s = document.createElement("div");
       (s.innerHTML =
         "<p>Opening Hours: </p> <p class='info-center'>Monday-Friday : 11:00 - 23:00</p><p class='info-center'>Saturday - Sunday: 10:00 - 23.30</p>"),
